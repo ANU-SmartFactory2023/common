36bdae8e43121ee49ea75a046adeefa8b5dec906
japgo <japgo@naver.com>
2023-12-02 13:06:59 +0900

    서버 통신 불가 시 fail 처리 되도록 수정.

--- server_communication.py
+++ server_communication.py
@@ -3,25 +3,31 @@ import http.client
 import json
 
 #적외선 센서와 2차 센서 역할이 다름으로 모델 추가 및 변경
-from model import SensorModel
-from model import ProcessModel
+from models import SensorModel
+from models import ProcessModel
 
 # 클래스의 메서드에서 첫 번째 매개변수로 self를 사용하는 것은 파이썬의 규칙 중 하나입니다. 
 # self를 사용하여 클래스의 인스턴스 변수 및 메서드에 접근할 수 있다.
 class ServerComm :
     conn:http.client.HTTPConnection
     headers = {"Content-type": "application/json", "Accept": "*/*"}
+    fail_msg = '{"msg":"fail","statusCode":"500"}' 
     
     #__init__ 설정 메서드에 서버 ip주소 및 포트번호 설정
     def __init__( self ) :
         self.conn = http.client.HTTPConnection( '192.168.1.12', 5000 ) # 서버 ip, 포트
+        self.conn.timeout = 3
 
     # HTTP 통신 Sensor Post 정의
     def sensorRequestPost( self, url, s:SensorModel ) :
-        self.conn.request( 'POST', url, json.dumps( s.__dict__ ), self.headers )
-        result = self.conn.getresponse().read().decode()
-        json_object = json.loads( result )     
-
+        result = ""
+        try:
+            self.conn.request( 'POST', url, json.dumps( s.__dict__ ), self.headers )
+            result = self.conn.getresponse().read().decode()
+        except:
+            result = self.fail_msg
+               
+        json_object = json.loads( result )  
         # json 안 답변 분리 후 변수 저장 가능
         msg = json_object[ 'msg' ] 
         statusCode = json_object[ 'statusCode' ]   
@@ -30,12 +36,16 @@ class ServerComm :
 
     # HTTP 통신 Process Post 정의
     def ProcessRequestPost( self, url, p:ProcessModel ) :
-        # p 클래스 변수들을 딕셔너리 형태로 변환 후 전송
-        self.conn.request( 'POST', url, json.dumps( p.__dict__ ), self.headers )
-        # getresponse()를 호출하면 http.client.HTTPResponse 객체 반환
-        # read() 메서드를 호출하여 응답 데이터를 읽고
-        # decode()를 사용하여 해당 데이터를 문자열로 디코딩
-        result = self.conn.getresponse().read().decode()
+        result = ""
+        try:
+            # p 클래스 변수들을 딕셔너리 형태로 변환 후 전송
+            self.conn.request( 'POST', url, json.dumps( p.__dict__ ), self.headers )
+            # getresponse()를 호출하면 http.client.HTTPResponse 객체 반환
+            # read() 메서드를 호출하여 응답 데이터를 읽고
+            # decode()를 사용하여 해당 데이터를 문자열로 디코딩
+            result = self.conn.getresponse().read().decode()
+        except:
+            result = self.fail_msg
 
         # json.loads 함수를 사용하여 이를 파이썬 객체로 변환한다.
         json_object = json.loads( result )  
@@ -60,8 +70,13 @@ class ServerComm :
 
     # HTTP 통신 Get 정의
     def requestGet( self, url ) :
-        self.conn.request( 'GET', url  )
-        result = self.conn.getresponse().read().decode()
+        result = ""
+        try:
+            self.conn.request( 'GET', url  )
+            result = self.conn.getresponse().read().decode()
+        except:
+            result = self.fail_msg
+            
         json_object = json.loads( result )
 
         return json_object
